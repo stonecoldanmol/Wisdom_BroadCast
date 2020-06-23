fef7c83bb2f2f035e17cc500912ae2060da5bcd4
AnmolSinha <sinhaanmol.16@gmail.com>
2020-06-23 15:12:58 +0530

    Final

---
+++ lib/helper/news.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutterapp/models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News{
+  List<ArticleModel> news =[];
+
+ Future<void> getNews() async{
+   String url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=13c151ddebfe4d8ca390454e5359bdd1";
+
+   var response = await http.get(url);
+
+   var jsonData = jsonDecode(response.body);
+
+   if(jsonData['status'] == "ok"){
+    jsonData["articles"].forEach((element){
+
+      if(element["urlToImage"] != null && element["description"] != null){
+
+        ArticleModel articleModel= ArticleModel(
+        title: element['title'],
+          author: element["author"],
+          description: element["description"],
+          url: element["url"],
+          urlToImage: element["urlToImage"],
+          content: element["context"]
+        );
+
+        news.add(articleModel);
+
+      }
+
+
+    });
+   }
+
+
+ }
+}
+
+class CategoryNewsClass{
+  List<ArticleModel> news =[];
+
+  Future<void> getNews(String category) async{
+    String url = "https://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=13c151ddebfe4d8ca390454e5359bdd1";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if(jsonData['status'] == "ok"){
+      jsonData["articles"].forEach((element){
+
+        if(element["urlToImage"] != null && element["description"] != null){
+
+          ArticleModel articleModel= ArticleModel(
+              title: element['title'],
+              author: element["author"],
+              description: element["description"],
+              url: element["url"],
+              urlToImage: element["urlToImage"],
+              content: element["context"]
+          );
+
+          news.add(articleModel);
+
+        }
+
+
+      });
+    }
+
+
+  }
+}
